c5cbd94b1ab5f46014dc36018cd2208054a1541b
Ars_Nikon <nik.arseniy01@gmail.com>
2022-12-11 20:18:22 +0300

    add export to xlsx

---
+++ MyAppDevextremeAspCoreProject/wwwroot/js/OnExport.js
@@ -0,0 +1,15 @@
+﻿function exporting(e) {
+    var workbook = new ExcelJS.Workbook();
+    var worksheet = workbook.addWorksheet('Employees');
+
+    DevExpress.excelExporter.exportDataGrid({
+        component: e.component,
+        worksheet: worksheet,
+        autoFilterEnabled: true
+    }).then(function () {
+        workbook.xlsx.writeBuffer().then(function (buffer) {
+            saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Employees.xlsx');
+        });
+    });
+    e.cancel = true;
+}
